d37c5ad4433bbb4d8e1427ec72f3d32fc99d0a20
Ksyusha88 <ksyusha.gorelova@gmail.com>
2021-02-28 02:32:43 +0200

    gallery

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import './styles.css';
 import articlesTpl from "./tamplete/articles.hbs";
 import apiService from './js/apiService.js';
+import * as basicLightbox from 'basiclightbox';
+require('basiclightbox/dist/basicLightbox.min.css');
+// import { error, success } from '@pnotify/core/dist/PNotify.js';
+// const { defaults } = require('@pnotify/core');
 
 const refs = {
     searchForm: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery-container'),
-    loadMoreBtn: document.querySelector('.js-load-more')
+    loadMoreBtn: document.querySelector('.js-load-more'),
+    galleryContainer: document.querySelector('.gallery-container'),
+    query: document.querySelector('.query')
 }
 
 refs.searchForm.addEventListener('submit', event =>{
@@ -17,9 +23,13 @@ refs.searchForm.addEventListener('submit', event =>{
     form.reset(); //чистим форму 
 
     apiService.resetPage();
+    refs.loadMoreBtn.classList.add('is-hidden');
+
 
     apiService.fetchGallery().then(hits =>{
         updatePic(hits);
+    refs.loadMoreBtn.classList.remove('is-hidden');
+    scrollElem();   
     });
 });
 
@@ -30,11 +40,29 @@ function updatePic(hits) {
     refs.gallery.insertAdjacentHTML('beforeend', markup)
 }
 
-
-
-
 refs.loadMoreBtn.addEventListener('click', () =>{
     apiService.fetchGallery(searchQuery).then(hits => {
         updatePic(hits);
     });
 });
+
+function modalLightbox() {
+    const instance = basicLightbox.create(`
+      <div class="modal">
+      <img src="${event.target.dataset.source}" >
+      </div>
+  `);
+    instance.show();
+  }
+  refs.gallery.addEventListener('click', modalLightbox);
+
+  function scrollElem() {
+      window.scrollTo({
+        top: document.documentElement.offsetHeight,
+        behavior: 'smooth',
+      });
+    }
+
+  
+
+  
